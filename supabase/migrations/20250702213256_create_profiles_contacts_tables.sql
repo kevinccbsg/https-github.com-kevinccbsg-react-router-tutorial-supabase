6eacaa419b4b56e2dfb03dce7475efcade5bd86f
kevinccbsg <kevinccbsg@gmail.com>
2025-07-02 23:48:48 +0200

    feat: add first migration

---
+++ supabase/migrations/20250702213256_create_profiles_contacts_tables.sql
@@ -0,0 +1,123 @@
+create table "public"."contacts" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "first_name" text not null,
+    "last_name" text not null,
+    "username" text not null,
+    "email" text not null,
+    "phone" text not null,
+    "favorite" boolean not null,
+    "avatar" text,
+    "profile_id" uuid not null
+);
+
+
+alter table "public"."contacts" enable row level security;
+
+create table "public"."profiles" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "email" text not null
+);
+
+
+alter table "public"."profiles" enable row level security;
+
+CREATE UNIQUE INDEX contacts_pkey ON public.contacts USING btree (id);
+
+CREATE UNIQUE INDEX profiles_pkey ON public.profiles USING btree (id);
+
+alter table "public"."contacts" add constraint "contacts_pkey" PRIMARY KEY using index "contacts_pkey";
+
+alter table "public"."profiles" add constraint "profiles_pkey" PRIMARY KEY using index "profiles_pkey";
+
+alter table "public"."contacts" add constraint "contacts_profile_id_fkey" FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE not valid;
+
+alter table "public"."contacts" validate constraint "contacts_profile_id_fkey";
+
+grant delete on table "public"."contacts" to "anon";
+
+grant insert on table "public"."contacts" to "anon";
+
+grant references on table "public"."contacts" to "anon";
+
+grant select on table "public"."contacts" to "anon";
+
+grant trigger on table "public"."contacts" to "anon";
+
+grant truncate on table "public"."contacts" to "anon";
+
+grant update on table "public"."contacts" to "anon";
+
+grant delete on table "public"."contacts" to "authenticated";
+
+grant insert on table "public"."contacts" to "authenticated";
+
+grant references on table "public"."contacts" to "authenticated";
+
+grant select on table "public"."contacts" to "authenticated";
+
+grant trigger on table "public"."contacts" to "authenticated";
+
+grant truncate on table "public"."contacts" to "authenticated";
+
+grant update on table "public"."contacts" to "authenticated";
+
+grant delete on table "public"."contacts" to "service_role";
+
+grant insert on table "public"."contacts" to "service_role";
+
+grant references on table "public"."contacts" to "service_role";
+
+grant select on table "public"."contacts" to "service_role";
+
+grant trigger on table "public"."contacts" to "service_role";
+
+grant truncate on table "public"."contacts" to "service_role";
+
+grant update on table "public"."contacts" to "service_role";
+
+grant delete on table "public"."profiles" to "anon";
+
+grant insert on table "public"."profiles" to "anon";
+
+grant references on table "public"."profiles" to "anon";
+
+grant select on table "public"."profiles" to "anon";
+
+grant trigger on table "public"."profiles" to "anon";
+
+grant truncate on table "public"."profiles" to "anon";
+
+grant update on table "public"."profiles" to "anon";
+
+grant delete on table "public"."profiles" to "authenticated";
+
+grant insert on table "public"."profiles" to "authenticated";
+
+grant references on table "public"."profiles" to "authenticated";
+
+grant select on table "public"."profiles" to "authenticated";
+
+grant trigger on table "public"."profiles" to "authenticated";
+
+grant truncate on table "public"."profiles" to "authenticated";
+
+grant update on table "public"."profiles" to "authenticated";
+
+grant delete on table "public"."profiles" to "service_role";
+
+grant insert on table "public"."profiles" to "service_role";
+
+grant references on table "public"."profiles" to "service_role";
+
+grant select on table "public"."profiles" to "service_role";
+
+grant trigger on table "public"."profiles" to "service_role";
+
+grant truncate on table "public"."profiles" to "service_role";
+
+grant update on table "public"."profiles" to "service_role";
+
+
+
