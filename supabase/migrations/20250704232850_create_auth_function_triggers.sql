e14c49134b0a991887a32431227a05094471884a
kevinccbsg <kevinccbsg@gmail.com>
2025-07-05 01:37:32 +0200

    feat: add migration for handling user

---
+++ supabase/migrations/20250704232850_create_auth_function_triggers.sql
@@ -0,0 +1,22 @@
+alter table "public"."profiles" add constraint "profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."profiles" validate constraint "profiles_id_fkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO ''
+AS $function$
+begin
+  insert into public.profiles (id, email)
+  values (new.id, new.raw_user_meta_data ->> 'email');
+  return new;
+end;
+$function$
+;
+
+
+
